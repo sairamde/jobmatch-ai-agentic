f9d89af00841041d3c18232146f72c94ec95f725
SAIRAM1603 <deshettysairam0115@gmail.com>
2026-03-20 17:15:52 +0530

    Fixed Gemini import for Streamlit

--- utils/gemini.py
+++ utils/gemini.py
@@ -4,6 +4,6 @@ import google.generativeai as genai
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 def ask_llm(prompt):
-    model = genai.GenerativeModel("gemini-pro")
+    model = genai.GenerativeModel("gemini-2.5-flash")
     response = model.generate_content(prompt)
     return response.text
